dfb51c4e35c5deaa2f6a504fb966c0cd4cd011df
Tram Pham <tracy@users-MacBook-Pro.local>
2022-03-15 19:39:30 +1100

    build up api

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,10 @@
-import Head from 'next/head';
 import React from 'react';
+import Meta from '../components/Meta';
 
 const about = () => {
   return (
     <div>
-      <Head>
-        <title>About</title>
-        <meta name='keywords' content='web development, programming' />
-      </Head>
+      <Meta title='About'/>
       <h1>About</h1>
     </div>
   );
